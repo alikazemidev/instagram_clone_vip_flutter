185c9f6bc5764ec01544bb5f15532976d469d19b
Ali kazemi <alikazemidev@gmail.com>
2022-08-24 15:09:11 +0430

    first commit & create splash screen

---
+++ android/app/src/main/kotlin/com/example/instagram_clone_vip_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.instagram_clone_vip_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        // color: Color(0xff1c1f2e),
+        image: DecorationImage(
+          repeat: ImageRepeat.repeat,
+          image: Image.asset('images/pattern1.png').image,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(
+          alignment: AlignmentDirectional.bottomCenter,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 80),
+              child: Center(
+                child: Image.asset(
+                  'images/logo_splash.png',
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 32,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'from',
+                    style: TextStyle(color: Colors.grey, fontSize: 16),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Expert flutter vip',
+                    style: TextStyle(color: Colors.blue, fontSize: 18),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
